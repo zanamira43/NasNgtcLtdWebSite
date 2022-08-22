090b505e9b4b007f72ae75f9fa88833269917bd0
zanamira43 <zanamira43@gmail.com>
2022-08-22 18:30:11 +0300

    modified scroll and link

--- components/Ku/Partner.vue
+++ components/Ku/Partner.vue
@@ -23,13 +23,13 @@
 <script setup>
 
 const pImages = [
-  '/public/partner/partner1.svg',
-  '/public/partner/partner2.svg',
-  '/public/partner/partner3.svg',
-  '/public/partner/partner4.svg',
-  '/public/partner/partner5.svg',
-  '/public/partner/partner6.svg',
-  '/public/partner/partner7.svg'
+  '../../public/partner/partner1.svg',
+  '../../public/partner/partner2.svg',
+  '../../public/partner/partner3.svg',
+  '../../public/partner/partner4.svg',
+  '../../public/partner/partner5.svg',
+  '../../public/partner/partner6.svg',
+  '../../public/partner/partner7.svg'
 ]
 
 </script>
