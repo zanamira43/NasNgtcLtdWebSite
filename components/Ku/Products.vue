eb0ef6011f7dbc3bab6c6aab898910c72a21b3fc
zanamira43 <zanamira43@gmail.com>
2022-08-28 11:30:35 +0300

    addtion solar section

--- components/Ku/Products.vue
+++ components/Ku/Products.vue
@@ -1,5 +1,5 @@
 <template >
-  <div id="kuProduct" class="bg-[#06A4DF] my-0 p-10 bg-opacity-[20%]">
+  <div id="kuProduct" class="bg-[#06A4DF] my-0 p-10 bg-opacity-[20%] h-auto">
     <div class="container mx-auto">
       <!-- title && sub-title section -->
       <div class="flex flex-col items-center space-y-1 my-5 w-full">
@@ -13,14 +13,14 @@
 
       <!-- partner logo or image section -->
       <div class="mt-10">
-        <div class="grid lg:grid-cols-5 md:grid-cols-5 grid-cols-1 gap-2">
-          <div v-for="product in products" :key="product.id">
-             <div class="w-full  h-[80%] border border-gray-300">
+        <div class="grid lg:grid-cols-5 md:grid-cols-5 grid-cols-1 gap-3">
+          <div v-for="product in products" :key="product.id" class="mt-5">
+             <div class="w-full h-auto">
               <NuxtLink :to="`/kurdish/${product.name}`">
-                <img :src="product.image" :alt="product.name" class="w-full h-full" />
+                <img :src="product.image" :alt="product.name" class="w-full h-[250px]" />
               </NuxtLink>
              </div>
-             <div class="flex flex-col items-center py-2">
+             <div class="flex flex-col items-center py-3">
               <p class="font-bold text-xl text-gray-800">{{product.name}}</p>
              </div>
           </div>
