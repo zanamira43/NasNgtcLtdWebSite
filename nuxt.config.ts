a4b0c1428c0dc4368070019d5db556eccade2bb0
zanamira43 <zanamira43@gmail.com>
2022-09-21 14:17:32 +0300

    set robots.txt in public directory

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
     css: ['/assets/main.css', '@fortawesome/fontawesome-free/css/all.css'],
     modules: ['@nuxtjs/tailwindcss'],
     postcss: {
